81b58ee3b2f9604e6112d69930ac5818ac445342
Aleksandr Matviienko <alexander.matvienko@devit-team.com>
2022-01-30 19:30:35 +0200

    first commit

---
+++ mynote_client/pages/index.tsx
@@ -0,0 +1,9 @@
+import type {NextPage} from 'next'
+import Note from "../components/Note/Note";
+import MainLayout from "../layout/MainLayout";
+
+const Home: NextPage = () => <MainLayout><Note/></MainLayout>
+
+export default Home
+
+

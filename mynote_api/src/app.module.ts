5f07ed3ddbefcaa07c0bacc6d2a835621982062a
Your Name <you@example.com>
2022-02-04 16:49:23 +0200

    download data from boostnote

--- mynote_api/src/app.module.ts
+++ mynote_api/src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+const url = process.env.MONGO_URL || 'localhost';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(`mongodb://${url}:27017/my_note`),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
